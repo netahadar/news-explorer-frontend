335f4353ce48088ccec7390f888a3e8ec3a3b1ab
Neta Hadar Itzhak <netook6@gmail.com>
2022-01-11 14:51:54 +0200

    create About component

---
+++ news-explorer/src/components/About/About.js
@@ -0,0 +1,28 @@
+import React from "react";
+import mePath from "../../images/me.jpg";
+
+export default function About() {
+  return (
+    <>
+      <img className="about__image" src={mePath} alt="Neta Hadar Itzhak"></img>
+      <div className="about__container">
+        <h2 className="about__title">About the author</h2>
+        <p className="about__text">
+          Hi! My name is Neta-Hadar Itzhak, A Yandex Full Stack Boot Camp
+          student (at the final project stage) from Jerusalem.
+        </p>
+        <p className="about__text">
+          Graduated from Offensive Cyber Security and Python programming course.
+        </p>
+        <p className="about__text">
+          Volunteering as deputy director of the Cyber Department at “HEMA”-
+          Emergency Organization for saving Children and Youth at Risk.
+        </p>
+        <p className="about__text">
+          I am a great believer in independent learning and self-enrichment, and
+          looking for a Junior Full Stack Developer position.
+        </p>
+      </div>
+    </>
+  );
+}
--- news-explorer/src/components/Main/Main.js
+++ news-explorer/src/components/Main/Main.js
@@ -2,16 +2,22 @@ import React from "react";
 import "./Main.css";
 import SearchForm from "../SearchForm/SearchForm";
 import backgroundPath from "../../images/searchFormBackground.png";
+import About from "../About/About";
 
 export default function Main() {
   return (
-    <section className="search">
-      <div
-        className="search__container"
-        style={{ backgroundImage: `url("${backgroundPath}")` }}
-      >
-        <SearchForm />
-      </div>
-    </section>
+    <>
+      <section className="search">
+        <div
+          className="search__container"
+          style={{ backgroundImage: `url("${backgroundPath}")` }}
+        >
+          <SearchForm />
+        </div>
+      </section>
+      <section className="about">
+        <About />
+      </section>
+    </>
   );
 }
