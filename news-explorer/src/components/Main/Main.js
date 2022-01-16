fd9b13d7201f48bea979a09a3dacad78131787da
Neta Hadar Itzhak <netook6@gmail.com>
2022-01-16 15:36:11 +0200

    create NotFound component

--- news-explorer/src/components/Main/Main.js
+++ news-explorer/src/components/Main/Main.js
@@ -4,6 +4,7 @@ import SearchForm from "../SearchForm/SearchForm";
 import backgroundPath from "../../images/searchFormBackground.png";
 import About from "../About/About";
 import Preloader from "../Preloader/Preloader";
+import NotFound from "../NotFound/NotFound";
 
 export default function Main() {
   return (
@@ -18,6 +19,9 @@ export default function Main() {
       </section>
       {/* <section className="circle-preloader">
         <Preloader />
+      </section> */}
+      {/* <section className="not-found">
+        <NotFound />
       </section> */}
       <section className="about">
         <About />
---
+++ news-explorer/src/components/NotFound/NotFound.js
@@ -0,0 +1,18 @@
+import React from "react";
+import notFoundPath from "../../images/notFound.svg";
+
+export default function NotFound() {
+  return (
+    <div className="not-found__container">
+      <img
+        className="not-found__image"
+        src={notFoundPath}
+        alt="sad smiley"
+      ></img>
+      <h2 className="not-found__title">Nothing found</h2>
+      <p className="not-found__text">
+        Sorry, but nothing matched your search terms.
+      </p>
+    </div>
+  );
+}
